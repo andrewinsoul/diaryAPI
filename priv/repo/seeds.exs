af1b41a66f074d0c174b2a44b3edfcdc7703193e
Nature Okoye <andrewinsoul@gmail.com>
2024-07-20 01:07:14 +0100

    remove static folder from been ignored by git

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+schema = DiaryAPI.Accounts.User
+path_from_schema = to_string(schema) |> String.replace("Elixir", "") |> String.replace(".", "/")
+IO.puts("ignore >>>>>>> /lib#{path_from_schema}")
+{:ok, file_contents} = File.read("./lib#{path_from_schema}.ex")
+# ans1 = String.c
+find_required_fields = String.slice(file_contents, "cast(attrs,")
+IO.puts(find_required_fields)
+{position, length} = file_contents |> :binary.match("cast(attrs,")
+
+IO.puts(position)
+IO.puts(length)
