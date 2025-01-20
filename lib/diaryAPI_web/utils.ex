4ed9dbea4c7dfedb79718d294a751c02d677678c
Nature Okoye <andrewinsoul@gmail.com>
2025-01-20 01:33:53 +0100

    add specs to some util functions

--- lib/diaryAPI_web/utils.ex
+++ lib/diaryAPI_web/utils.ex
@@ -1,6 +1,8 @@
 import Ecto.Query, only: [where: 3], warn: false
 
 defmodule DiaryAPIWeb.Utils do
+  alias DiaryAPI.Diaries.Diary
+
   def translate_errors(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
       Gettext.dgettext(DiaryAPIWeb.Gettext, "errors", msg, opts)
@@ -14,10 +16,12 @@ defmodule DiaryAPIWeb.Utils do
     |> String.replace("Bearer ", "")
   end
 
+  @spec decode_token(binary()) :: {:error, any()} | {:ok, map()}
   def decode_token(token) do
     DiaryAPI.Guardian.decode_and_verify(token)
   end
 
+  @spec filter_out_soft_delete_col(Diary) :: Ecto.Query.t()
   def filter_out_soft_delete_col(query) do
     query |> where([q], is_nil(q.deleted_at))
   end
